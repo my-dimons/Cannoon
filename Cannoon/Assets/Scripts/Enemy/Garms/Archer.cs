bf360130a2508aa4026071c9bb639f13c88a9a65
mydimons <mydimons.kb@gmail.com>
2025-06-06 16:00:59 -0700

    Archer Enemy
    Archer enemy

---
+++ Cannoon/Assets/Scripts/Enemy/Garms/Archer.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : MonoBehaviour
+{
+    public GameObject arrow;
+    public AnimationClip attackAnim;
+    public Vector2 arrowSpawnPos;
+    public float cooldown;
+    public float attackTime;
+    public bool canAttack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Cooldown());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canAttack)
+        {
+            StartCoroutine(ShootArrow());
+            StartCoroutine(GetComponent<FollowEnemyAI>().FreezeEnemy(attackAnim.length));
+        }
+    }
+
+    IEnumerator Cooldown()
+    {
+        canAttack = false;
+        yield return new WaitForSeconds(cooldown);
+        canAttack = true;
+    }
+    IEnumerator ShootArrow()
+    {
+        StartCoroutine(Cooldown());
+        GetComponent<FollowEnemyAI>().animator.SetBool("isAttacking", true);
+
+        yield return new WaitForSeconds(attackTime);
+
+
+        GetComponent<FollowEnemyAI>().animator.SetBool("isAttacking", false);
+        if (GetComponent<FollowEnemyAI>().facingRight)
+        {
+            GameObject spawnedArrow = Instantiate(arrow, new(transform.position.x - arrowSpawnPos.x, transform.position.y + arrowSpawnPos.y, transform.position.z), transform.rotation);
+            spawnedArrow.GetComponent<Bullet>().SetStats(30, 1, 25);
+            spawnedArrow.GetComponent<Bullet>().sprite.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            GameObject spawnedArrow = Instantiate(arrow, new(transform.position.x + arrowSpawnPos.x, transform.position.y + arrowSpawnPos.y, transform.position.z), transform.rotation);
+            spawnedArrow.GetComponent<Bullet>().SetStats(-30, 1, 25);
+        }
+
+    }
+}
